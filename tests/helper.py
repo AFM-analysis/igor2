bf0a7f1d41ca79d0d462b2842dbea54e49aa845f
TeetotalingTom <thomas@vanwaas.org>
2024-06-15 12:10:01 +0200

    Updated compatibilities, helper, and tests

--- tests/helper.py
+++ tests/helper.py
@@ -1,10 +1,36 @@
 import pathlib
-from pprint import pformat
+
+from pprint import pformat, PrettyPrinter
+import numpy as _numpy
+
+class NumpyPrettyPrinter(PrettyPrinter):
+    def format(self, object, context, maxlevels, level):
+        if isinstance(object, _numpy.ndarray):
+            # Generate the entire array string using numpy's own mechanism but ensuring dtype is displayed
+            array_repr = _numpy.array2string(object, separator=', ')
+            # Append dtype only once for the whole array
+            formatted_array = f"array({array_repr}, dtype='{object.dtype}')"
+            return (formatted_array, True, False)
+        # Default handling for other types
+        return super().format(object, context, maxlevels, level)
+
+
+def custom_pformat(object, indent=1, width=80, depth=None, *, compact=False):
+    printer = NumpyPrettyPrinter(indent=indent, width=width, depth=depth, compact=compact)
+    return printer.pformat(object)
+
+def format_data(data):
+    lines = custom_pformat(data).splitlines()
+    return '\n'.join([line.rstrip() for line in lines])
+
+
+# def format_data(data):
+#     lines = pformat(data).splitlines()
+#     return '\n'.join([line.rstrip() for line in lines])
 
 from igor2.binarywave import load as loadibw
 
 data_dir = pathlib.Path(__file__).parent / "data"
-
 
 def assert_equal_dump_no_whitespace_no_byte(data_a, data_b):
     def repl(x):
@@ -12,11 +38,12 @@ def assert_equal_dump_no_whitespace_no_byte(data_a, data_b):
             [" ", ""],  # ignore whitespaces
             ["b'", "'"],  # ignore bytes vs str
             ["\n", ""],  # ignore newlines
-            # teat all floats as equal
+            # treat all floats as equal
             ["float32", "float"],
             ["float64", "float"],
             ["'>f4'", "float"],
             ["'>f8'", "float"],
+            ["'float'", "float"],
         ]:
             x = x.replace(old, new)
         return x
@@ -32,11 +59,6 @@ def dumpibw(filename):
     path = data_dir / filename
     data = loadibw(path)
     return format_data(data)
-
-
-def format_data(data):
-    lines = pformat(data).splitlines()
-    return '\n'.join([line.rstrip() for line in lines])
 
 
 def walk_callback(dirpath, key, value):
